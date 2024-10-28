a5e88ef3a7ca993a96e1f0d1b039d08a39626c27
Maxime Mahdavian <maxime.mahdavian@ssc-spc.gc.ca>
2024-10-28 13:58:12 -0400

    Added outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,18 @@
+output "front-door-object" {
+  description = "Outputs the entire front door object"
+  value = azurerm_cdn_frontdoor_profile.frontdoor_profile
+}
+
+output "front-door-id" {
+  description = "Outputs the front door profile ID"
+  value = azurerm_cdn_frontdoor_profile.frontdoor_profile.id
+}
+
+output "front-door-name" {
+  description = "Outputs the front door profile name"
+  value = azurerm_cdn_frontdoor_profile.frontdoor_profile
+}
+
 
 
 
