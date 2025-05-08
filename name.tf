a4c0045eef9c387e88ef1ce75279ecd34692e8d7
Maxime Mahdavian <maxime.mahdavian@ssc-spc.gc.ca>
2025-05-08 13:35:24 -0400

    Fixing typo in the naming convention

--- name.tf
+++ name.tf
@@ -1,7 +1,7 @@
 locals {
   name_regex = "/[//\"'\\[\\]:|<>+=;,?*@&]/" # Can't include those characters  name: \/"'[]:|<>+=;,?*@&
   env_4                         = substr(var.env, 0, 4)
-  userDefinedString_7           = substr(var.userDefinedString, 0, (7 + length(var.project)))
+  userDefinedString_7           = substr(var.userDefinedString, 0, 64 - (7 + length(var.project)))
   front-door-name                = replace("${local.env_4}-${var.project}-${local.userDefinedString_7}", local.name_regex, "")
   rule_set-name =               replace("${local.front-door-name}", "-", "")
 
